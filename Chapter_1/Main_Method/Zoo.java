7ca59a612692868131aa6304daac1752cefddc23
Will Parks <parkswdev@outlook.com>
2021-04-09 09:37:58 -0400

    Chapter 1

--- Chapter_1/Main_Method/Zoo.java
+++ Chapter_1/Main_Method/Zoo.java
@@ -1,11 +1,39 @@
 package Chapter_1.Main_Method;
 
 public class Zoo {
+    /**
+     * Java doc example
+     * @author Will Parks
+     * @param args description about the parameter
+     * @return description about the thing being returned
+     */
     public static void main(String[] args) {
-        System.out.println("Hello from the Brooklyn Zoo!");
-
-        // You can pass parameters to a Java Program
         System.out.println(args[0]);
         System.out.println(args[1]);
+
+
+        /*
+        in the cli...
+        java Zoo Bronx Zoo
+        -----Output-----
+        Hello from the Brooklyn Zoo!
+        Bronx Zoo
+        */
+
+        /*
+        java Zoo.java "San Diego" Zoo
+        ----Output----
+        San Diego
+        Zoo
+         */
+
+        /*
+        java Zoo.java San Diego Zoo
+        ----Output----
+        San
+        Diego
+
+        ...and the third parameter is ignored.
+         */
     }
 }
